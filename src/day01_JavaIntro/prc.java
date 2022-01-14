4f166177266bc96630b10098d93d1e530e932a31
aege06 <aysegulkucuk06006@gmail.com>
2022-01-14 11:44:23 -0600

    Initial commit

---
+++ src/day01_JavaIntro/prc.java
@@ -0,0 +1,37 @@
+package day01_JavaIntro;
+
+public class prc {
+    public static void main(String[] args) {
+
+/*String ta="A";
+ta=ta.concat("B");
+String tb="C";
+ta=ta+tb;
+ta.replace('C','D');
+ta=ta+tb;
+        System.out.println(ta);
+
+        char grade='A';
+        boolean passed = grade=='A' ||grade=='B';
+        boolean passed2= grade=='C' || grade=='D';
+        if(passed||passed2){
+            System.out.println("You passed");
+        }else {
+            System.out.println("Fail");
+        }
+
+  int x=10;
+  int y=x++;
+        System.out.println(y);
+        System.out.println(y++ +" "+ x++ +" " +y);*/
+
+        int z=5;
+        for (int i = 5; i >0; i--) {
+            z+=i;
+
+        }
+        System.out.println(z);
+        }
+}
+
+
---
+++ src/day23_CustomMethods_Void/example.java
@@ -0,0 +1,25 @@
+package day23_CustomMethods_Void;
+
+public class example {
+    public static void main(String[] args) {
+
+        float    a   =  100.987_6543f;
+
+        short  b  =  (byte) a ;
+
+        byte  c   = (byte) b;
+
+        System.out.println(c);
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
